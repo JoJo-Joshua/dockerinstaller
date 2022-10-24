631afceacf3c490580870d7185c600ead48d5794
JoJo Joshua <70030233+JoJo-Joshua@users.noreply.github.com>
2022-10-25 00:25:44 +0200

    Update Dockerinstaller.sh

--- Dockerinstaller.sh
+++ Dockerinstaller.sh
@@ -14,7 +14,7 @@ echo "##################################################"
 echo ""
 echo ""
 
-sleep 5
+sleep 2
 
 apt update -y
 
@@ -24,13 +24,9 @@ apt install git -y
 
 apt install curl -y
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-
-sudo sh get-docker.sh
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+chmod +x /usr/local/bin/docker-compose
 
 rm -r get-docker.sh
 rm -r Dockerinstaller.sh
